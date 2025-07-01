ed02c86923dde1dae6e655a7fcadcaddba9998ea
Moataz <moataz_abdullah@outlook.com>
2025-07-01 11:13:53 +0300

    added some custom css colors for the theme switch ,fixed some bugs and made a condition that changes the bg color of body and sub input

--- javascript/index.js
+++ javascript/index.js
@@ -1,2 +1,25 @@
-var toggleBtn = document.querySelector("#toggleBtn");
+var toggleBtn = document.querySelectorAll(".toggleBtn");
 var allElements = document.querySelectorAll("*");
+var sun = document.querySelectorAll(".sun");
+var moon = document.querySelectorAll(".moon");
+
+toggleBtn.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    toggleLightTheme();
+  });
+});
+
+function toggleLightTheme() {
+  sun.forEach((icon) => icon.classList.toggle("d-none"));
+  moon.forEach((icon) => icon.classList.toggle("d-none"));
+
+  allElements.forEach((el) => {
+    if (el.classList.contains("bg-secondary")) {
+      el.classList.remove("bg-secondary");
+      el.classList.add("bg-light-body");
+    } else if (el.classList.contains("bg-light-body")) {
+      el.classList.remove("bg-light-body");
+      el.classList.add("bg-secondary");
+    }
+  });
+}
